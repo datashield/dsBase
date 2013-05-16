b8f424009a3963d7dde80b1ed495fd9203975246
Amadou Gaye <ahmet_gaye@yahoo.fr>
2013-05-16 20:59:31 +0100

    Function amended by adding base:: to call R base functions

--- R/density.grid.ag.R
+++ R/density.grid.ag.R
@@ -7,12 +7,11 @@
 
 density.grid.ag <- function(xvect,yvect)
 {
-  
   xvect.save<-xvect
   yvect.save<-yvect
   
-  xvect.not.missing<-1-(is.na(xvect)*1)
-  yvect.not.missing<-1-(is.na(yvect)*1)
+  xvect.not.missing<-1-(base::is.na(xvect)*1)
+  yvect.not.missing<-1-(base::is.na(yvect)*1)
   
   x.and.y.present<-xvect.not.missing*yvect.not.missing
   
@@ -22,45 +21,43 @@ density.grid.ag <- function(xvect,yvect)
   
   
   #par(mfrow=c(1,1))
-  y.min<-min(yvect)
-  x.min<-min(xvect)
-  y.max<-max(yvect)
-  x.max<-max(xvect)
+  y.min<-base::min(yvect)
+  x.min<-base::min(xvect)
+  y.max<-base::max(yvect)
+  x.max<-base::max(xvect)
   
   
-  y.range<-max(yvect)-min(yvect)
-  x.range<-max(xvect)-min(xvect)
+  y.range<-base::max(yvect)-base::min(yvect)
+  x.range<-base::max(xvect)-base::min(xvect)
   
   numints<-20
   y.interval<-y.range/numints
   x.interval<-x.range/numints
   
-  y.cuts<-seq(from=y.min,to=y.max,by=y.interval)
-  y.mids<-seq(from=(y.min+y.interval/2),to=(y.max-y.interval/2),by=y.interval)
+  y.cuts<-base::seq(from=y.min,to=y.max,by=y.interval)
+  y.mids<-base::seq(from=(y.min+y.interval/2),to=(y.max-y.interval/2),by=y.interval)
   y.cuts[numints+1]<-y.cuts[numints+1]*1.001
   
-  x.cuts<-seq(from=x.min,to=x.max,by=x.interval)
-  x.mids<-seq(from=(x.min+x.interval/2),to=(x.max-x.interval/2),by=x.interval)
+  x.cuts<-base::seq(from=x.min,to=x.max,by=x.interval)
+  x.mids<-base::seq(from=(x.min+x.interval/2),to=(x.max-x.interval/2),by=x.interval)
   x.cuts[numints+1]<-x.cuts[numints+1]*1.001
   
   
-  grid.density<-matrix(0,nrow=numints,ncol=numints)
+  grid.density<-base::matrix(0,nrow=numints,ncol=numints)
   
   for(j in 1:numints)
   {
     for(k in 1:numints)
     {
-      grid.density[j,k]<-sum(1*(yvect>=y.cuts[j] & yvect<y.cuts[j+1] & xvect >=x.cuts[k] & xvect<x.cuts[k+1]))
+      grid.density[j,k]<-base::sum(1*(yvect>=y.cuts[j] & yvect<y.cuts[j+1] & xvect >=x.cuts[k] & xvect<x.cuts[k+1]), na.rm=TRUE)
     }
   }
-  print(length(x.mids))
-  print(length(y.mids))
+  base::print(base::length(x.mids))
+  base::print(base::length(y.mids))
   
-  #grid.density.obj<-data.frame(matrix(grid.density),as.vector(x.mids),as.vector(y.mids))
-  grid.density.obj<-cbind(grid.density,x.mids,y.mids)
+  #grid.density.obj<-data.frame(base::matrix(grid.density),as.vector(x.mids),as.vector(y.mids))
+  grid.density.obj<-base::cbind(grid.density,x.mids,y.mids)
   
   return(grid.density.obj)
-  
-  
 }
 
