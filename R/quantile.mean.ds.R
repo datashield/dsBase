87fba1ad85f28bbbcae27c31b15df9b33c4ab364
Amadou Gaye <ahmet_gaye@yahoo.fr>
2013-05-16 20:41:27 +0100

    Function amended

--- R/quantile.mean.ds.R
+++ R/quantile.mean.ds.R
@@ -4,8 +4,8 @@
 #' @export
 #' 
 quantile.mean.ds <- function (a) {
-  qq <- base::quantile(a,c(0.05,0.1,0.25,0.5,0.75,0.9,0.95), na.rm=TRUE)
-  mm <- base::mean(a,na.rm=TRUE)
+  qq <- stats::quantile(a,c(0.05,0.1,0.25,0.5,0.75,0.9,0.95), na.rm=TRUE)
+  mm <- stats::mean(a,na.rm=TRUE)
   quantile.obj <- c(qq, mm)
   
   names(quantile.obj) <- c("5%","10","25%","50%","75%","90%","95%","Mean")
