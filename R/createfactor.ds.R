0fb15548e729c6f3a835858fab64faf8b7613537
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-10-09 12:44:21 +0100

    Amended 'createfactor.ds', added 'isValid.ds' and amended the description file to add some base functions.

--- R/createfactor.ds.R
+++ R/createfactor.ds.R
@@ -4,10 +4,7 @@
 #' The generate factor is checked and if valid (no category with count > 0 and < 5)
 #' returned; if not valid an empty of the same length is returned. 
 #' @param xvect a numerical or character vector
-#' @param levels optional vector of values that 'xvect' might have taken
-#' @param labels labels for the different levels
-#' @param exclude values to be excluded
-#' @param ordered tells if the variable 'x' is ordered
+#' @param numobs the number of observations/missing values to add to 'xvect'
 #' @return a 'valid' factor vector or an empty vector of the same length.
 #' @author Gaye, A.; Burton, P.
 #' @export
@@ -28,32 +25,17 @@
 #' datashield.aggregate(opals, quote(length(sex)))
 #'  }
 #'
-createfactor.ds <- function (xvect, levels=NULL, labels=levels, exclude=NA, ordered=is.ordered(xvect)){
+createfactor.ds <- function (xvect=NULL, numobs=NULL){
   
-  if(is.null(levels)){
-    tt <- table(xvect)
-    levels <- names(tt)
-    labels <- levels
+  if(is.null(xvect)){
+    Stop("\n\nPlease provide a valid numeric or factor vector!\n\n")
   }
-  # use the R function to turn the input vector into a factor
-  # if it is not already a factor
-  if(!(is.factor(xvect))){
-    xvector <- factor(xvect, levels, labels, exclude, ordered)
-  }else{
-    xvector <- xvect
+  if(is.null(numobs)){
+    Stop("\n\nNumber of observations must provided!\n\n")
   }
   
-  # call the function that checks of the vector is a valid factor
-  out <- checkfactor.ds(xvector) 
-  status <- out$status
-  
-  # if the factor is valid (i.e. no categories with between 0 and 5 counts)
-  # return the created factor otherwise print a failure message
-  if(status == 0){
-    fvect <- xvector 
-  }else{
-    fvect <- rep(NA, length(xvector))
-  }
-  cat(length(fvect),"\n")
-  return(fvect)
+  # add the required number of observations as missing values
+  newxvect <- factor(c(as.character(xvect), rep(NA,numobs)))
+
+  return(newxvect)
 }
---
+++ R/isValid.ds.R
@@ -0,0 +1,46 @@
+#'
+#' @title Checks if an input is valid
+#' @details This function checks if an input variable is valid. A numeric variable
+#' is not valid if it has > 0 and < 5 observations and a factor variable is not valid if
+#' any of its levels (classes) has a count of between 1 and 4 
+#' @param xvect a numeric or factor vector
+#' @return a boolean TRUE if input is valid or stops the process otherwise
+#' @author Gaye, A.
+#' @export
+#' @examples 
+#' \dontrun{
+#' # load the file that contains the login details
+#' data(logindata)
+#' 
+#' # login and assign a variable to R
+#' library(opal)
+#' opals <- datashield.login(logins=logindata,assign=TRUE,variables=list("LAB_HDL"))
+#'
+#' # check if the variable LAB_HDL is valid
+#' datashield.aggregate(opals, quote(summary.ds(D$LAB_HDL)))
+#'}
+#'
+isValid.ds <- function(xvect) {
+  
+  if(class(xvect) == "numeric") {
+    if(length(xvect) > 0 & length(xvect)  <5) {
+      stop("The input vector is not valid!\nThe variable has between 1 and 4 observations.\n\n")
+    } else {
+      return(TRUE)
+    }
+  }else{
+    if(class(xvect) == "factor"){
+      tt <- tabulate(xvect)
+      xx <- which(tt > 0 & tt < 5)
+      if(length(xx) > 0) {
+        stop("The input vector is not valid!\nOne or more levels have a count of between 1 and 4.\n\n")
+      } else {
+        return(TRUE)
+      }
+    }else{
+      Stop("\n\nThe variable is not a numeric or a factor!\n\n")
+    }
+  } 
+  
+
+}
