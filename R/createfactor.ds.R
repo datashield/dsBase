9e83782ff4cedcfbc4bee86983b048eb3c9bd6de
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-10-10 23:23:47 +0100

    Amended function 'createfactor.ds'.

--- R/createfactor.ds.R
+++ R/createfactor.ds.R
@@ -1,10 +1,11 @@
 #' 
 #' @title Generates a factor variable
 #' @description The functions uses the input factor and generates a new factor 
-#' with additional levels.
-#' @param xvect a numerical or character vector
-#' @param addLevels the additional levels to add to the input factor
-#' @return a factor vector that has its original levels plus the added levels
+#' with new levels.
+#' @param xvect a factor vector
+#' @param categories the levels of the  factor to generate
+#' @return a factor vector that has the levels of the original vector 
+#' and some additional levels
 #' @author Gaye, A.
 #' @export
 #' @examples 
@@ -14,28 +15,29 @@
 #' 
 #' # login and assign specific variable(s)
 #' library(opal)
-#' myvar <- list("GENDER")
+#' myvar <- list("PM_BMI_CATEGORICAL")
 #' opals <- datashield.login(logins=logindata,assign=TRUE,variables=myvar)
 #' 
-#' # generate a factor vector using the variable 'GENDER'
-#' datashield.assign(opals, "sex", quote(createfactor.ds(D$GENDER)))
+#' # get the levels of variable PM_BMI_CATEGORICAL in study1
+#' current.levels <- datashield.aggregate(opals[1], quote(levels(D$PM_BMI_CATEGORICAL)))
 #' 
-#' # display the number of observation in each vector
-#' datashield.aggregate(opals, quote(length(sex)))
+#' #' generate a new factor vector which has an additonal level '4' 
+#' newlevels <- c(current.levels[[1]], "4")
+#' datashield.assign(opals, "bmi_f", quote(createfactor.ds(D$PM_BMI_CATEGORICAL, newlevels)))
+#'
 #'  }
 #'
-createfactor.ds <- function (xvect=NULL, addLevels=NULL){
+createfactor.ds <- function (xvect=NULL, categories=NULL){
   
   if(is.null(xvect)){
-    Stop("\n\nPlease provide a valid numeric or factor vector!\n\n")
+    stop("\n\nPlease provide a valid numeric or factor vector!\n\n")
   }
-  if(is.null(addLevels)){
-    Stop("\n\nNumber of observations must provided!\n\n")
+  if(is.null(categories)){
+    stop("\n\nthe levels of the variable must provided!\n\n")
   }
   
   # get the current levels of the input vector
-  old.levels <- levels(xvect)
-  levels(xvect) <- c(old.levels, addLevels)
+  levels(xvect) <- categories
 
   return(xvect)
 }
