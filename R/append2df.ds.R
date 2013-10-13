951831f1116699ddf442a1b672984c34bb5d5728
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-10-13 17:38:58 +0100

    'append2df.ds' amended.

--- R/append2df.ds.R
+++ R/append2df.ds.R
@@ -1,7 +1,7 @@
 #'
 #' @title Appends a column(s) to a dataframe
 #' @param dataset a dataframe
-#' @param column a column/vector to append to the dataframe
+#' @param column a list which contains the column/vector to append to the dataframe
 #' @return a new dataframe is assigned to the datasources
 #' @author Gaye, A.
 #' @export
@@ -21,19 +21,24 @@
 #' # center LAB_HDL for each study
 #' for(i in 1:length(opals)){
 #'   call.object <- call("-", quote(D$LAB_HDL), mean.lab_hdl[[i]])
-#'   datashield.assign(opals[i], "lab_hdl.c", cally)
+#'   datashield.assign(opals[i], "lab_hdl.c", call.object)
 #' }
 #' 
 #' # now append 'lab_hdl.c' to the initially assigned dataframe 'D'
 #' # and replace the initial dataframe 'D' by the new dataframe
-#' datashield.assign(opals, "Dnew", quote(append2df.ds(D,lab_hdl.c)))
+#' datashield.assign(opals, "Dnew", quote(append2df.ds(D,list("lab_hdl.c"=lab_hdl.c))))
 #' datashield.assign(opals, "D", quote(Dnew))
 #' }
 #'
 append2df.ds <- function (dataset, column) {
   
   if(is.data.frame(dataset)){
-    new.dataset <- data.frame(dataset, column)
+    new.dataset <- data.frame(dataset, column[[1]])
+    if(is.null(names(column)[[1]])){
+      cat("Warning: no name provided for the appended column!")
+    }else{
+    colnames(new.dataset)[length(new.dataset)] <- names(column)[1]
+    }
     return(new.dataset)
   }else{
     stop("\n\''xvect'dataset' muxt be a dataframe!\n\n")
