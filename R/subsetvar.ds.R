c1b071e794a9e69cff1d06f6ef88f544b5ce1eca
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-10-15 10:55:37 +0100

    Old version of 'subsetvar.ds' put back in.

--- R/subsetvar.ds.R
+++ R/subsetvar.ds.R
@@ -1,10 +1,10 @@
 #' 
 #' @title Derives a valid binary variable from a continuous variable
 #' @details This function runs only if the set threshold (i.e. >=)
-#' is met or not met by more than one observation.
+#' is met or not met by 1 single observation.
 #' @param xvect a continuous numeric variable
-#' @param operator a string character that represents the logical operator.
-#' Six logical operators are possible: '>', '<', '>=', '<=', '==' and '!='.
+#' @param operator an integer that sets the operator 1 for '>', 2 for '<', 
+#' 3 for '>=', 4 for '<=',  5 for '==' and 6 for '!='
 #' @param threshold a numeric value that sets the threshold
 #' @return a vector where the entries that met the condition are 
 #' assigned the value 1 or 0 otherwise
@@ -19,11 +19,14 @@
 #' myvar <- list("PM_BMI_CONTINUOUS")
 #' opals <- datashield.login(logins=logindata,assign=TRUE,variables=myvar)
 #' 
-#' # get a subset of PM_BMI_CONTINUOUS with the '>=70' 
-#' #' call.object <-  call("subsetvar.ds", quote(D$PM_BMI_CONTINUOUS), quote(as.character(">=")), quote(as.numeric(70)))
-#' datashield.assign(opals, "subset", call.object)
+#' # run the function with for the condition '>=70'
+#' var <- quote(D$PM_BMI_CONTINUOUS) 
+#' operator <- 3
+#' threshold <- 70
+#' call.object <-  call("subsetvar.ds", var, as.numeric(operator), as.numeric(30))
+#' datashield.assign(opals, "bmi.>.30", call.object)
 #' }
-#' 
+#'  
 subsetvar.ds <- function(xvect=NULL, operator=NULL, threshold=NULL){
   
   if(is.null(xvect)){
@@ -39,8 +42,24 @@ subsetvar.ds <- function(xvect=NULL, operator=NULL, threshold=NULL){
   }
   
   # get te indices of the observation where the condition is true
-  cally <- call(operator, quote(xvect), threshold)
-  indx1 <- which(eval(cally))
+  if(operator == 1){
+    indx1 <- which(xvect > threshold)
+  }
+  if(operator == 2){
+    indx1 <- which(xvect < threshold)
+  }
+  if(operator == 3){
+    indx1 <- which(xvect >= threshold)
+  }
+  if(operator == 4){
+    indx1 <- which(xvect <= threshold) 
+  }
+  if(operator == 5){
+    indx1 <- which(xvect == threshold)
+  }
+  if(operator == 6){
+    indx1 <- which(xvect != threshold)
+  }
   
   # turn all the the value of all the observation that met the condition to 1
   xvect_out <- xvect
@@ -50,10 +69,10 @@ subsetvar.ds <- function(xvect=NULL, operator=NULL, threshold=NULL){
   # them to 0
   indx2 <- which(xvect_out != 1)
   xvect_out[indx2] <- 0
-
+  
   if(length(indx1) < 2 | length(indx2) < 2){
     stop("\n\nThe subset variable is not valid!\n\n")
   }else{
     return(xvect_out)
   }
-}
+} 
