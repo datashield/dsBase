12cf8c0edfd36dac27bab0832dfb02d9bf4987e7
Gaye <gaye@gaye-Latitude-D630.(none)>
2013-10-27 18:29:49 +0000

    'histogram.ds' amended.

--- R/histogram.ds.R
+++ R/histogram.ds.R
@@ -1,12 +1,13 @@
 #' 
 #' @title Computes a histogram of the given data values without plotting.
-#' @description this functions produces the information required to plot
+#' @description This functions produces the information required to plot
 #' a histogram. This is done without allowing for bins (cells) with a
 #' count of less than 5. If a bin has a count < 5 it is collapsed with 
 #' the nearing bin; this process iterates until all bins have count >=5.
 #' @param xvect the numeric vector for which the histogram is desired.
-#' @param brks a string character where the break values are concatenated by '_';
-#' this is because we can use the R function c() as an aggregate function.
+#' @param min a numeric, the lower limit of the distribution.
+#' @param max a numeric, the upper limit of the distribution.
+#' @param seed an integer, the value to set the seed at when generating the break points.
 #' @return a list with an object of class \code{histogram} and the number of invalid cells
 #' @export
 #' @author Gaye, A.
@@ -20,37 +21,64 @@
 #  myvar <- list("LAB_TSC")
 #' opals <- datashield.login(logins=logindata,assign=TRUE,variables=myvar)
 #' 
-#' # generate a histogram object without plotting
-#' hist.object <- datashield.aggregate(opals, quote(histogram.ds(D$LAB_TSC)))
+#' # get the range of the inout vector
+#' ranges <- datashield.aggregate(datasources, quote(range.ds(xvect)))
+#' minval <- ranges[[1]]
+#' maxval <- ranges[[2]]
+#' seedval <- 333
+#' # generate a histogram object
+#' hist.object <- datashield.aggregate(opals, quote(histogram.ds(xvect, minval, maxval, seedval)))
 #' }
 #' 
-histogram.ds <- function (xvect, brks) {
+histogram.ds <- function (xvect, min, max, seed) {
   
-  # turn the brks point given as a character into numricals
-  brks_n <- as.numeric(unlist(strsplit(brks, split="_")))
+  # the same seed is set in each study to ensure all studies have the same break points
+  set.seed(seed)
   
-  # get the histogram object
-  histout <- hist(xvect, breaks=brks_n, plot=FALSE)
+  # get the global break points and ensure that the breaks span the range of xvect.
+  gotbreaks <- TRUE
+  binwidth <- 0.3
+  brks <- round(seq(min, max, by=binwidth),4)
   
-  # check if any of the 'bins' contains a count < 5
-  indx <- which(histout$counts > 0 & histout$counts < 5)
-  l.small.counts <- length(indx)
-  
-  if(l.small.counts > 0){
-
-    # replace the corresponding, counts, densities and intensities by zeros
-    histout$counts[indx] <- 0
-    histout$density[indx] <- 0   
-    histout$intensities[indx] <- 0   
-    
-    # get the midpoints corresponding to the above indices
-    # these midpoint correspond to the invalid categories
-    invalidcells <- histout$mids[indx]
-  }else{
-    invalidcells <- NULL
+  if(min(brks) > min || max(brks) < max){
+    counter <- 0
+    while(min(brks) > min || max(brks) < max){
+      lastindx <- length(brks)
+      brks <- c( (brks[1]-binwidth), brks, (brks[lastindx]+binwidth) )
+      counter <- counter+1
+      if(counter >= 50){
+        gotbreaks <- FALSE
+      }
+    }
   }
   
-  # return a list with the histogram object and the vector 'axterispos'
-  return(list("histobject"=histout, "invalidcells"=length(invalidcells)))
+  # generate the histogram object if the breaks points were obtained
+  if(gotbreaks){
+    # get the histogram object
+    histout <- hist(xvect, breaks=brks_n, plot=FALSE)
+    
+    # check if any of the 'bins' contains a count < 5
+    indx <- which(histout$counts > 0 & histout$counts < 5)
+    l.small.counts <- length(indx)
+    
+    if(l.small.counts > 0){
+  
+      # replace the corresponding, counts, densities and intensities by zeros
+      histout$counts[indx] <- 0
+      histout$density[indx] <- 0   
+      histout$intensities[indx] <- 0   
+      
+      # get the midpoints corresponding to the above indices
+      # these midpoint correspond to the invalid categories
+      invalidcells <- histout$mids[indx]
+    }else{
+      invalidcells <- NULL
+    }
+    
+    # return a list with the histogram object and the vector 'axterispos'
+    return(list("histobject"=histout, "invalidcells"=length(invalidcells)))
+  }else{
+    return(NULL)
+  }
   
 }
