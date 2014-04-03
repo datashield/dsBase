cff7b14dc2b992828e82a7dffad1e9c88ab3ae9c
agaye <ag13748@it017324.(none)>
2014-04-03 19:53:45 +0100

    'var.ds' removed and 'varDS' added. 'meanDS' amended. The changes was to ensure an 'NA' is returned when the inut vector is invalid instead of stopping.

--- R/varDS.R
+++ R/varDS.R
@@ -1,10 +1,10 @@
 #'
-#' @title Computes statistical mean of vector with more than 4 entries
-#' @description Calculates the mean value.
+#' @title Computes the variance of vector with more than 4 entries
+#' @description Calculates the variance.
 #' @details if the length of input vector is less than the set filter
 #' a missing value is returned. 
 #' @param xvect a vector
-#' @return a numeric, the statistical mean
+#' @return a numeric, the variance
 #' @author Gaye, A.
 #' @export
 #' @examples 
@@ -17,18 +17,19 @@
 #  myvar <- list("LAB_TSC")
 #' opals <- datashield.login(logins=logindata,assign=TRUE,variables=myvar)
 #' 
-#' # compute the statistical mean
-#' stat.mean <- datashield.aggregate(opals, quote(meanDS(D$LAB_TSC)))
+#' # compute the variance
+#' variance <- datashield.aggregate(opals, quote(varDS(D$LAB_TSC)))
 #' }
 #'
-meanDS <- function (xvect) {
+varDS <- function (xvect) {
   # this filter sets the minimum number of observations that are allowed 
   nfilter <- dsbase:::.setFilterDS()
   
   if(length(xvect) < nfilter){
     result <- NA
   }else{
-    result <- mean(xvect, na.rm=TRUE) 
+    result <- var(xvect, na.rm=TRUE) 
   }
+  
   return(result)
 }
