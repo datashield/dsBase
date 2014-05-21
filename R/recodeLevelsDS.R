e59bab2b5e30df21794d5483fbd81df6d18c171a
agaye <ag13748@it017324.(none)>
2014-05-21 23:52:30 +0100

    Typo corrected

--- R/recodeLevelsDS.R
+++ R/recodeLevelsDS.R
@@ -2,13 +2,13 @@
 #' @title Recodes the levels of a categorical variables
 #' @description The functions uses the input factor and generates a new factor 
 #' with new levels.
-#' @param xvect a factor vector
+#' @param x a factor vector
 #' @param classes a character vector the levels of the  newt factor vector
 #' @return a factor vector with the new levels
 #' @author Gaye, A.
 #' @export
 #'
-recodelevelsDS <- function (xvect=NULL, classes=NULL){
+recodeLevelsDS <- function (x=NULL, classes=NULL){
   
   # check if the input vector is valid (i.e. respect DataSHIELD conditions)
   check <- isValid.ds(xvect)
