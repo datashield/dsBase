1c0c701818bcd465bb2d1966ac47c904ca0255cf
agaye <ag13748@it017324.(none)>
2014-05-22 01:04:12 +0100

    'isValidDS' made internal function.

--- R/isValidDS.R
+++ R/isValidDS.R
@@ -1,29 +1,20 @@
 #'
 #' @title Checks if an input is valid
-#' @description This function checks if an input variable is valid. A numeric variable
+#' @description Internal function
+#' @details This function checks if an input variable is valid. A numeric variable
 #' is not valid if it has > 0 and < 5 observations and a factor variable is not valid if
 #' any of its levels (classes) has a count of between 1 and 4.
 #' @param xvect a numeric or factor vector.
 #' @return a boolean TRUE if input is valid or stops the process otherwise
 #' @author Gaye, A.
-#' @export
-#' @examples 
-#' \dontrun{
-#' library(opal)
-#' # load the file that contains the login details
-#' data(logindata)
-#' 
-#' # login and assign a variable to R
-#' opals <- datashield.login(logins=logindata,assign=TRUE,variables=list("LAB_HDL"))
 #'
-#' # check if the variable LAB_HDL is valid
-#' datashield.aggregate(opals, quote(isValid.ds(D$LAB_HDL)))
-#'}
-#'
-isValidDS <- function(xvect) {
+.isValidDS <- function(xvect) {
+  
+  # this filter sets the minimum number of observations that are allowed 
+  nfilter <- dsbase:::.setFilterDS()
   
   if(class(xvect) == "numeric") {
-    if(length(xvect) > 0 & length(xvect)  <5) {
+    if(length(xvect) > 0 & length(xvect)  < nfilter) {
       stop("The input vector is not valid!\nThe variable has between 1 and 4 observations.\n\n")
     } else {
       return(TRUE)
@@ -31,7 +22,7 @@ isValidDS <- function(xvect) {
   }else{
     if(class(xvect) == "factor"){
       tt <- tabulate(xvect)
-      xx <- which(tt > 0 & tt < 5)
+      xx <- which(tt > 0 & tt < nfilter)
       if(length(xx) > 0) {
         return(FALSE)
         stop("The input vector is not valid!\nOne or more levels have a count of between 1 and 4.\n\n")
