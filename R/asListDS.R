341c6130bf87d9557a5002897e439bf519732f97
agaye <ag13748@it017324.(none)>
2014-05-22 17:13:33 +0100

    Sorted...hopefully.

--- R/asListDS.R
+++ R/asListDS.R
@@ -7,27 +7,27 @@
 #' @export
 #' 
 asListDS <-function (input){
-#   # check if the input is a dataframe or a matrix
-#   # this should be checked on the client side
-#   # but just in case a '***' changes the client code
-#   # to 'hack' he/she will not be able to reach here
-#   cl <- class(input)
-#   if(cl != 'data.frame' & cl != 'matrix'){
-#     output <- as.list(rep(NA, 4))
-#   }else{
-#     # check if the input vector is valid (i.e. meets DataSHIELD criteria)
-#     check <- dsbase:::.isValidDS(input)
-#     if(check){
-#       if(cl == 'matrix'){ 
-#         input2 <- as.data.frame(input) 
-#       }else{
-#         input2 <- input
-#       }
-      output <- as.list(input)
-#       names(output) <- colnames(input2)
-#     }else{
-#       output <- as.list(rep(NA, 4))
-#     }
-#   }
+  # check if the input is a dataframe or a matrix
+  # this should be checked on the client side
+  # but just in case a '***' changes the client code
+  # to 'hack' he/she will not be able to reach here
+  cl <- class(input)
+  if(cl != 'data.frame' & cl != 'matrix'){
+    output <- as.list(rep(NA, 4))
+  }else{
+    # check if the input vector is valid (i.e. meets DataSHIELD criteria)
+    check <- dsbase:::.isValidDS(input)
+    if(check){
+      if(cl == 'matrix'){ 
+        input2 <- as.data.frame(input) 
+      }else{
+        input2 <- input
+      }
+      output <- as.list(input2)
+      names(output) <- colnames(input2)
+    }else{
+      output <- as.list(rep(NA, 4))
+    }
+  }
   return(output)
 }
