a7d95536e82c9047c2dd0e76ce2e8840e9a30bd1
agaye <ag13748@it017324.(none)>
2014-06-27 15:19:36 +0100

    'rowColCalc' renamed and code amended.

--- R/rowColCalcDS.R
+++ R/rowColCalcDS.R
@@ -1,8 +1,9 @@
 #' 
 #' @title Computes sums and means of rows or columns of numeric arrays
 #' @description The function is similar to R base functions 'rowSums', 'colSums',
-#' 'rowMeans' and 'colMeans' but its outcome cannot be returned to the user as it might
-#' be revealing if the array contains only one row or one column. 
+#' 'rowMeans' and 'colMeans'.
+#' @details the output is returned to the user only the number of entries in the 
+#' output vector is greater or equal to the allowed size. 
 #' @param dataset an array of two or more dimensions.
 #' @param operation an integer that indicates the operation to carry out:
 #' 1 for 'rowSums', 2 for 'colSums', 3 for 'rowMeans' or 4 for 'colMeans'
@@ -24,7 +25,7 @@
 #' datashield.assign(opals, "rowsums", call.object)
 #' }
 #' 
-rowcolCalc.ds <- function (dataset, operation) {
+rowColCalcDS <- function (dataset, operation) {
   
   if(operation == 1){
     result <- rowSums(dataset, na.rm=TRUE)
@@ -37,6 +38,14 @@ rowcolCalc.ds <- function (dataset, operation) {
   }
   if(operation == 4){
     result <- colMeans(dataset, na.rm=TRUE)
+  }
+  
+  # check if the output is valid (i.e. meets DataSHIELD criteria)
+  check <- isValidDS(result)
+  if(check){
+    return(result)
+  }else{
+    return(rep(NA, 4))
   }
   
   return(result)
