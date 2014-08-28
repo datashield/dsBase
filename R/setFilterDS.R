f77cd2ff003ce8ccec584cde21893b5d7464de66
agaye <ag13748@it017324.(none)>
2014-08-28 19:25:11 +0100

    Chnage of code to allow the privacy level to be set in opal.

--- R/setFilterDS.R
+++ R/setFilterDS.R
@@ -1,14 +1,15 @@
 #' 
 #' @title Sets the privacy level
 #' @description This is an internal function that set the number of observation
-#' that are allowed in any table or vector
+#' that are allowed in any table or vector. The function gets the value from opal
+#' and if no value was set in opal it uses the default value 5.
 #' @details The function is called by server side functios
 #' @param x a dummy argument
 #' @return an integer between 1 and 5
 #' @keywords internal
 #' @author Gaye, A.
 #'
-setFilterDS <- function(x=5){
-  a <- x
-  return(5)
+setFilterDS <- function(x=getOption("datashield.privacyLevel", default=5)){
+  a <- as.numeric(as.character(x))
+  return(a)
 }
