3921bddc29767f169ddd947ab0a3b387fb74e45d
agaye <ag13748@it017324.(none)>
2014-11-20 00:52:54 +0000

    'numNaDS' added to aggregate functions.

---
+++ R/numNaDS.R
@@ -0,0 +1,14 @@
+#' 
+#' @title Counts the number of missing values
+#' @description this function just counts the number of missing entries 
+#' in a vector. 
+#' @param xvect a vector
+#' @return an integer, the number of missing values
+#' @author Gaye, A.
+#' @export
+#'
+numNaDS <- function(xvect){
+  
+  out <- length(which(is.na(xvect)))
+  return (out)
+}
