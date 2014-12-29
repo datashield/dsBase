880347799a15d1ab130f357fbfe6491275b132d4
agaye <ag13748@it017324.(none)>
2014-12-28 23:31:55 -0500

    'table1dDS' and 'table2dDS' re-written from scratch.

--- R/table1dDS.R
+++ R/table1dDS.R
@@ -1,56 +1,40 @@
 #'
-#' @title Creates 1-dimensional contingency tables - potentially disclosive data are suppressed
-#' @description The function table1d.ds is a server-side subfunction of datashield.aggregate(). 
-#' It generates 1-dimensional tables for all data sources. Valid (non-disclosive) data are defined 
-#' as data from sources where no table cells have counts between 1 and 4 (the upper value [4] can in 
-#' principle be changed but only by rewriting the underlying function - it cannot be changed by a 
-#' standard DataSHIELD user). If the count in any cell in the table produced by a given data source 
-#' IS invalid, that cell count is changed to "-1" and the name of the category that corresponds to it is 
-#' changed to "-9". Each source is flagged as having only valid data, or at least some invalid data. 
-#' Missing data are treated as na.action=na.omit (default for table function), if missing data are to be treated as
-#' a separate and visible category, the variable should first be transformed to convert NAs to the a new value.
-#' @param xvect a numerical vector with discrete values - usually a factor
-#' @return A list object (eg. named out.obj) with one component from each separate study which contains the following items:
-#' \item{is.table.valid}{eg. out.obj[[2]]$is.table.valid is a logical (Boolean) indicator of whether the data in study 2 is 
-#' entirely valid (TRUE) for the stated variable or at least one category is invalid (FALSE)}
-#' \item{safe.table}{eg. out.obj[[1]]$safe.table contains the safe (non-disclosive) one dimensional table that can be released 
-#' from study 1 - unsafe data are concealed by converting invalid counts to "-1" and their corresponding catregory identifier to "-9"}
-#' @author Burton, P.
+#' @title Creates 1-dimensional contingency tables
+#' @description This function generates a 1-dimensional table where potentially disclosive cells.
+#' (based on the set threshold) are replaced by a missing value ('NA').
+#' @details It generates a 1-dimensional tables where valid (non-disclosive) 1-dimensional tables are defined 
+#' as data from sources where no table cells have counts between 1 and the set threshold. When the ouput table
+#' is invalid all cells but the total count are replaced by missing values. Only the total count is visible 
+#' on the table returned to the client site. A message is also returned with the 1-dimensional; the message 
+#' says "invalid table - invalid counts present" if the table is invalid and 'valid table' otherwise.
+#' @param xvect a numerical vector with discrete values - usually a factor.
+#' @return a list which contains two elements: 'table', the 1-dimensional table and 'message' a message which
+#' informs about the validity of the table.
+#' @author Gaye A.
 #' @export
 #'
 table1dDS  <- function(xvect){
 
-  # get the character name of the variable
-  var.name.1_os2=deparse(substitute(xvect))
+  # tabulate the input vector and output the result in a data frame format
+  aa <- t(as.data.frame((table(xvect))))
+  bb <- as.data.frame(t(as.numeric(aa[2,]))) 
+  
+  # add the total count and colnames names
+  cc <- cbind(bb, sum(bb[1,], na.rm=TRUE))
+  colnames(cc) <- c(aa[1,], "Total")
 
-  # assign the input variable 
-  factor_is1 <- xvect
+  # the minimum number of observations that are allowed (the below function gets the value from opal)
+  nfilter <- setFilterDS()
   
-  # assign name
-  var.name.1_is1 <- var.name.1_os2              
- 
-  # reset to K+1 if the non-allowable range is changed to 1 to K
-  critical.min <- setFilterDS()
-  
-  factor.valid <- TRUE
-  
-  table.new <- table(factor_is1)
-  
-  for(j in 1:length(table.new))
-  {
-    if(table.new[j] < critical.min && table.new[j] > 0)
-    {
-      table.new[j] <- (-1)
-      factor.valid <- FALSE
-      names(table.new)[j] <- (-9)
-    }
+  # check for invalid cells if any found change them to 'NA' and set the validity message accordingly
+  validity <- "valid Table"
+  indx <- which(cc[1,1:(dim(cc)[2] - 1)] > 0 & cc[1,1:(dim(cc)[2] - 1)] < nfilter)
+  if(length(indx) > 0){
+    cc[1,1:(dim(cc)[2] - 1)] <- NA
+    validity <- "invalid table - invalid counts present"
   }
-  names(dimnames(table.new)) <- paste("Categories/Counts for Variable:\n",var.name.1_is1)
-  names(factor.valid) <- paste(var.name.1_is1)
-  out.list_os1 <- list(factor.valid,as.table(table.new))
-  names(out.list_os1) <- list("is.factor.valid","safe.table")
   
-  # returns 1 key output object
-  return(out.list_os1)  
+  # return output table and message
+  return(list(table=cc, message=validity))
 }
 
--- R/table2dDS.R
+++ R/table2dDS.R
@@ -1,71 +1,48 @@
 #'
-#' @title Creates 2-dimensional contingency tables - potentially disclosive data are suppressed
-#' @description The function table2d.ds is a server-side subfunction of datashield.aggregate(). 
-#' It generates 2-dimensional tables for all data sources. Valid (non-disclosive) data are defined as data 
-#' from sources where no table cells have counts between 1 and 4 (the upper value [4] can in principle be 
-#' changed but only by rewriting the underlying function - it cannot be changed by a standard DataSHIELD user). 
-#' If the count in any cell in the table produced by a given data source IS invalid, that cell count is changed 
-#' to "-1" and the name of the two categories that correspond to it are both changed to "-9". Each source is flagged 
-#' as having only valid data, or at least some invalid data.Missing data are treated as na.action=na.omit 
-#' (default for table function), if missing data are to be treated as
-#' a separate and visible category, the variable should first be transformed to convert NAs to the a new value.
-#' @param xvect a numerical vector with discrete values - usually a factor
-#' @param yvect a numerical vector with discrete values - usually a factor
-#' @return A list object (eg. named out.obj) with one component from each separate study which contains the following items:
-#' \item{is.table.valid}{eg. out.obj[[2]]$is.table.valid is a logical (Boolean) indicator of whether the data in study 2 is 
-#' entirely valid (TRUE) for the stated variable or at least one category is invalid (FALSE)}
-#' \item{safe.table}{eg. out.obj[[1]]$safe.table contains the safe (non-disclosive) one dimensional table that can be released 
-#' from study 1 - unsafe data are concealed by converting invalid counts to "-1" and their corresponding catregory identifier to "-9"}
-#' @author Burton P.
+#' @title Creates 2-dimensional contingency tables
+#' @description This function generates a 2-dimensional table where potentially disclosive cells
+#' (based on the set threshold) are replaced by a missing value ('NA').
+#' @details It generates a 2-dimensional tables where valid (non-disclosive) 2-dimensional tables are defined 
+#' as data from sources where no table cells have counts between 1 and the set threshold. When the ouput table
+#' is invalid all cells but the total counts are replaced by missing values. Only the total counts are visible 
+#' on the table returned to the client site. A message is also returned with the 2-dimensional table; the message 
+#' says "invalid table - invalid counts present" if the table is invalid and 'valid table' otherwise.
+#' @param xvect a numerical vector with discrete values - usually a factor.
+#' @param yvect a numerical vector with discrete values - usually a factor.
+#' @return a list which contains two elements: 'table', the 1-dimensional table and 'message' a message which
+#' informs about the validity of the table.
+#' @author Gaye A.
 #' @export
 #' 
 table2dDS <- function(xvect,yvect){
 
-  # Assign vector of interest
-  vect1_os2 <- xvect
-  vect2_os2 <- yvect
-  
-  # assign character names to the variables
-  var.name.1_os2 <- "VAR.1"
-  var.name.2_os2 <- "VAR.2"
-  
-  # assign vector of interest to be factor_is1
-  factor1_is1 <- vect1_os2
-  factor2_is1 <- vect2_os2
-   
-  # assign its name from get.var.names.script
-  var.name.1_is1 <- var.name.1_os2
-  var.name.2_is1 <- var.name.2_os2
-
-  # reset to K+1 if the non-allowable range is changed to 1 to K  
-  critical.min <- setFilterDS()
-  
-  table.valid <- TRUE
-  
-  table.new <- table(factor1_is1,factor2_is1,dnn=c(var.name.1_is1,var.name.2_is1))
-  
-  nnr <- dim(table.new)[1]
-  nnc <- dim(table.new)[2]
-
-  for(j in 1:nnr)
-  {
-    for(k in 1:nnc)
-    {
-       if(table.new[j,k]<critical.min && table.new[j,k] > 0)
-        {
-        table.new[j,k] <- (-1)
-        table.valid <- FALSE
-        dimnames(table.new)[[1]][j] <- (-9)
-        dimnames(table.new)[[2]][k] <- (-9)
-        }
-    }
+  # tabulate the input vector and output the result in a data frame format
+  aa <- table(xvect, yvect)
+  bb <- matrix(NA, nrow=dim(aa)[1], ncol=dim(aa)[2])
+  for(i in 1:dim(aa)[1]){
+    bb[i,] <- aa[i,]
   }
-  # names(dimnames(table.new)) <- paste("Categories/Counts for Variable:\n",var.name.1_is1)
-  # names(table.valid) <- paste(var.name.1_is1,var.name.2_is1)
-  out.list_os1 <- list(table.valid,as.table(table.new))
-  names(out.list_os1) <- list("is.table.valid","safe.table")
+  bb <- rbind(bb, colSums(bb))
+  bb <- cbind(bb, rowSums(bb))
+  cc <- as.data.frame(bb)
+  colnames(cc) <- c(levels(yvect), "Total")
+  rownames(cc) <- c(levels(xvect), "Total")
+
+  # the minimum number of observations that are allowed (the below function gets the value from opal)
+  nfilter <- setFilterDS()
   
-  # returns 1 key output object
-  return(out.list_os1)
+  # check for invalid cells if any found change them to 'NA' and set the validity message accordingly
+  validity <- "valid Table"
+  for(i in 1: dim(cc)[2]){
+    indx <- which(cc[1:(dim(cc)[1] - 1),i] > 0 & cc[1:(dim(cc)[1] - 1),i] < nfilter)
+    if(length(indx) > 0){
+      cc[1:(dim(cc)[1] - 1), 1:(dim(cc)[2] - 1)] <- NA
+      validity <- "invalid table - invalid counts present"
+      break
+    }    
+  }
+
+  # return output table and message
+  return(list(table=cc, message=validity))
 }
 
