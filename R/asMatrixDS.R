9c25fa0a96357f7704d4a0c97129ea5f66286cd9
OllyButters <oliver.butters@gmail.com>
2019-07-31 12:24:19 +0100

    renamed all *.o.R -> *.R

--- R/asMatrixDS.R
+++ R/asMatrixDS.R
@@ -1,23 +1,28 @@
-#' 
-#' @title Attempts to convert the input into a matrix
-#' @description this function is similar to R function 'as.matrix'
-#' @details Unlike the r function 'as.matrix' some restriction are applied.
-#' @param input the object to turn into list.
-#' @return a valid list or an empty list
-#' @author Gaye, A.
+#' @title Coerces an R object into a matrix
+#' @description this function is based on the native R function {as.matrix}
+#' @details See help for function {as.matrix} in native R
+#' @param x.name the name of the input object to be coerced to class
+#' matrix. Must be specified in inverted commas. But this argument is
+#' usually specified directly by <x.name> argument of the clientside function
+#' {ds.asMatrix.o}
+#' @return the object specified by the <newobj> argument (or its default name
+#' <x.name>.mat) which is written to the serverside. For further
+#' details see help on the clientside function {ds.asMatrix.o} 
+#' @author Amadou Gaye, Paul Burton for DataSHIELD Development Team
 #' @export
-#' 
-asMatrixDS <-function (input){
-  
-  # check if the input is valid (i.e. meets DataSHIELD criteria)
-  check <- isValidDS(input)
-  if(check){
-    output <- as.matrix(input) 
-  }else{
-    output <- as.matrix(input) 
-    output[] <- NA
-  }
-  
+asMatrixDS.o <- function (x.name){
+
+if(is.character(x.name)){
+	x<-eval(parse(text=x.name))
+
+	}else{
+   studysideMessage<-"ERROR: x.name must be specified as a character string"
+   return(list(studysideMessage=studysideMessage))
+   } 
+
+  output <- as.matrix(x) 
+
   return(output)
-  
 }
+#ASSIGN FUNCTION
+# asMatrixDS.o
--- R/meanDS.R
+++ R/meanDS.R
@@ -5,20 +5,34 @@
 #' a missing value is returned. 
 #' @param xvect a vector
 #' @return a numeric, the statistical mean
-#' @author Gaye, A.
+#' @author Gaye A, Burton PR
 #' @export
 #'
-meanDS <- function (xvect) {
-  
-  # check if the input vector is valid (i.e. meets DataSHIELD privacy criteria)
-  check <- isValidDS(xvect)
-  
-  # return missing value if the input vector is not valid
-  if(check){
-    result <- mean(xvect, na.rm=TRUE) 
-  }else{
-    result <- NA
-  }
-  
-  return(result)
+meanDS.o <- function(xvect){
+
+  #############################################################
+  # MODULE 1: CAPTURE THE nfilter SETTINGS
+  thr <- listDisclosureSettingsDS.o()
+  nfilter.tab <- as.numeric(thr$nfilter.tab)
+  #nfilter.glm <- as.numeric(thr$nfilter.glm)
+  #nfilter.subset <- as.numeric(thr$nfilter.subset)
+  #nfilter.string <- as.numeric(thr$nfilter.string)
+  #############################################################
+
+  out.mean <- mean(xvect, na.rm=TRUE)
+  out.numNa <- length(which(is.na(xvect)))
+  out.totN <- length(xvect)
+  out.validN <- out.totN-out.numNa
+  studysideMessage <- "VALID ANALYSIS" 
+ 
+  if(out.validN < nfilter.tab){
+    out.mean <- NA
+    studysideMessage <- "FAILED: Nvalid less than nfilter.tab"
+  }   
+   
+  out.obj <- list(EstimatedMean=out.mean,Nmissing=out.numNa,Nvalid=out.validN,Ntotal=out.totN,ValidityMessage=studysideMessage)  
+  return(out.obj)	
+
 }
+#AGGREGATE FUNCTION
+# meanDS.o
