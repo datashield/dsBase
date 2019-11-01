2c3dd27e9eea65d69f1facb1fb3eeb78860ff641
Demetris Avraam <avraam.demetris@gmail.com>
2019-11-01 14:49:00 +0200

    change "\type" to "\code"

--- R/asIntegerDS.R
+++ R/asIntegerDS.R
@@ -1,15 +1,15 @@
 #' 
 #' @title Coerces an R object into class integer
-#' @description This function is based on the native R function type{as.integer}.
-#' @details See help for function \type{as.integer} in native R, and details section
-#' in the help file of the clientside function \type{ds.asInteger}.
+#' @description This function is based on the native R function \code{as.integer}.
+#' @details See help for function \code{as.integer} in native R, and details section
+#' in the help file of the clientside function \code{ds.asInteger}.
 #' @param x.name the name of the input object to be coerced to class
 #' integer. Must be specified in inverted commas. But this argument is
 #' usually specified directly by <x.name> argument of the clientside function
-#' \type{ds.asInteger}.
+#' \code{ds.asInteger}.
 #' @return the object specified by the <newobj> argument (or its default name
 #' <x.name>.int) which is written to the serverside. For further
-#' details see help on the clientside function \type{ds.asInteger}.
+#' details see help on the clientside function \code{ds.asInteger}.
 #' @author Amadou Gaye, Paul Burton, Demetris Avraam, for DataSHIELD Development Team
 #' @export
 #'
