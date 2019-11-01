9149de824894db18e97485ce5cb4fbc7883e53de
Demetris Avraam <avraam.demetris@gmail.com>
2019-11-01 14:50:12 +0200

    change "\type" to "\code"

--- R/asNumericDS.R
+++ R/asNumericDS.R
@@ -1,15 +1,15 @@
 #' 
 #' @title Coerces an R object into class numeric
-#' @description This function is based on the native R function \type{as.numeric}.
-#' @details See help for function \type{as.numeric} in native R, and details section
-#' in the help file of the clientside function type{ds.asNumeric}.
+#' @description This function is based on the native R function \code{as.numeric}.
+#' @details See help for function \code{as.numeric} in native R, and details section
+#' in the help file of the clientside function \code{ds.asNumeric}.
 #' @param x.name the name of the input object to be coerced to class
 #' numeric. Must be specified in inverted commas. But this argument is
 #' usually specified directly by <x.name> argument of the clientside function
-#' \type{ds.asNumeric}.
+#' \code{ds.asNumeric}.
 #' @return the object specified by the <newobj> argument (or its default name
 #' <x.name>.num) which is written to the serverside. For further
-#' details see help on the clientside function \type{ds.asNumeric}.
+#' details see help on the clientside function \code{ds.asNumeric}.
 #' @author Amadou Gaye, Paul Burton, Demetris Avraam, for DataSHIELD Development Team
 #' @export
 #'
