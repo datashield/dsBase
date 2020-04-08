d72ee087f9c352a9926ca8dfb9743f597b38aea7
LeireAbarrategui <60744058+LeireAbarrategui@users.noreply.github.com>
2020-04-08 14:19:41 +0100

    Update dataFrameSortDS.R

--- R/dataFrameSortDS.R
+++ R/dataFrameSortDS.R
@@ -98,7 +98,7 @@ dataFrameSortDS <- function(df.name=NULL,sort.key.name=NULL,sort.descending,sort
 
    if(sort.method=="alphabetic")
     {
-     key.order <- stringr::str_order(sort.key.2.use, decreasing = sort.descending, na_last = !sort.descending)
+     key.order <- stringr::str_order(sort.key.2.use, decreasing = sort.descending, na_last = TRUE)
     } else {
     key.order <- order(key.ranks)
     }
