cfbf37e0cc8b7eecf4fc5d2c10d1adbadb1268f5
LeireAbarrategui <60744058+LeireAbarrategui@users.noreply.github.com>
2020-04-16 11:40:12 +0100

    Update dataFrameSortDS.R

--- R/dataFrameSortDS.R
+++ R/dataFrameSortDS.R
@@ -98,7 +98,6 @@ dataFrameSortDS <- function(df.name=NULL,sort.key.name=NULL,sort.descending,sort
      }
       key.order <- order(key.ranks)
       }
-    }
     df.sorted <- df2sort[key.order,]  
     return(df.sorted)
 }
