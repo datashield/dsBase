a45ce1cfdd52e619b8850bba71005818bb8f95e9
Stuart Wheater <stuart.wheater@arjuna.com>
2020-05-14 13:46:32 +0100

    Added non-disclosure test

--- R/asFactorDS1.R
+++ R/asFactorDS1.R
@@ -1,6 +1,6 @@
-#'
+#' 
 #' @title Determines the levels of the input variable in each single study
-#' @description This function is an aggregate DataSHIELD function that returns the
+#' @description This function is an aggregate DataSHIELD function that returns the 
 #' levels of the input variable from each single study to the client-side function.
 #' @details The function encodes the input vector as factor and returns its levels in
 #' ascending order if the levels are numerical or in alphabetical order if the levels
@@ -10,9 +10,29 @@
 #' @export
 #'
 asFactorDS1 <- function(input.var.name=NULL){
+#########################################################################
+# DataSHIELD MODULE: CAPTURE THE nfilter SETTINGS                       #
+thr<-dsBetaTest::listDisclosureSettingsDS.o()                           #
+#nfilter.tab<-as.numeric(thr$nfilter.tab)                               #
+nfilter.glm<-as.numeric(thr$nfilter.glm)                                #
+#nfilter.subset<-as.numeric(thr$nfilter.subset)                         #
+#nfilter.string<-as.numeric(thr$nfilter.string)                         #
+#nfilter.stringShort<-as.numeric(thr$nfilter.stringShort)               #
+#nfilter.kNN<-as.numeric(thr$nfilter.kNN)                               #
+#datashield.privacyLevel<-as.numeric(thr$datashield.privacyLevel)       #
 
-  input.var <- eval(parse(text=input.var.name), envir = parent.frame())
+  input.var <- eval(parse(text=input.var.name))
   factor.levels.present.in.source <- levels(factor(input.var))
+  num.levels<-length(factor.levels.present.in.source)
+  max.allowed.levels<-length(input.var)*nfilter.glm
+  
+  if(num.levels>max.allowed.levels)
+  {
+	error.message<-
+	paste0("FAILED: this variable has too many levels and may be disclosive. The ds.asFactor function allows no more than",
+			max.allowed.levels,"levels in this particular study. This variable has",num.levels)
+	return(list(error.message=error.message))
+  }
 
   return(factor.levels.present.in.source)
 
