b56bf7c93a87581657b0f5fb52209e904147efe5
Stuart Wheater <stuart.wheater@arjuna.com>
2020-08-04 18:24:10 +0100

    More server-side tests

---
+++ tests/testthat/test-smk-rmDS.R
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019-2020 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+context("rmDS::smk::setup")
+
+set.standard.disclosure.settings()
+
+#
+# Tests
+#
+
+context("rmDS::smk::data.frame")
+test_that("simple rmDS, numeric data.frame", {
+#    input <- "value"
+
+#    res <- rmDS("input")
+
+#    expect_equal(class(res), "list")
+#    expect_length(res, 1)
+#    expect_equal(res$return.message, "")
+})
+
+#
+# Done
+#
+
+context("rmDS::smk::shutdown")
+
+context("rmDS::smk::done")
