c8c9bce8ee9a86fe4c308dba2edfe044c507595a
Stuart Wheater <stuart.wheater@arjuna.com>
2020-08-12 13:20:43 +0100

    Extended 'subsetByClassDS' tests

--- tests/testthat/test-smk-subsetByClassDS.R
+++ tests/testthat/test-smk-subsetByClassDS.R
@@ -21,7 +21,7 @@ set.standard.disclosure.settings()
 #
 
 context("subsetByClassDS::smk")
-test_that("simple subsetByClassDS, unspecified variables", {
+test_that("simple subsetByClassDS, data.frame, unspecified variables", {
     data      <- data.frame(v1 = factor(c(0, 0, 0, 1, 1, 1, 2, 1, 2, 2)), v2 = c(4.0, 0.0, 3.0, 1.0, 2.0, 2.0, 1.0, 3.0, 0.0, 4.0), v3 = c(1:10), v4 = c(1:10))
     variables <- NULL
 
@@ -40,7 +40,7 @@ test_that("simple subsetByClassDS, unspecified variables", {
     expect_length(res$v1.level_2, 4)
 })
 
-test_that("simple subsetByClassDS, specified variables", {
+test_that("simple subsetByClassDS, data.frame, specified variables", {
     data      <- data.frame(v1 = factor(c(0, 0, 0, 1, 1, 1, 2, 1, 2, 2)), v2 = c(4.0, 0.0, 3.0, 1.0, 2.0, 2.0, 1.0, 3.0, 0.0, 4.0), v3 = c(1:10), v4 = c(1:10))
     variables <- c("v1")
 
@@ -57,6 +57,25 @@ test_that("simple subsetByClassDS, specified variables", {
 
     expect_equal(class(res$v1.level_2), "data.frame")
     expect_length(res$v1.level_2, 4)
+})
+
+test_that("simple subsetByClassDS, factor vector, specified variables", {
+    data      <- factor(c(0, 0, 0, 1, 1, 1, 2, 1, 2, 2))
+    variables <- NULL
+
+    res <- subsetByClassDS("data", variables)
+
+    expect_equal(class(res), "list")
+    expect_length(res, 3)
+
+    expect_equal(class(res$data.level_0), "factor")
+    expect_length(res$data.level_0, 3)
+
+    expect_equal(class(res$data.level_1), "factor")
+    expect_length(res$data.level_1, 4)
+
+    expect_equal(class(res$data.level_2), "factor")
+    expect_length(res$data.level_2, 3)
 })
 
 #
