2382d307d194fdb2eefbef57a56a54175b24e609
Stuart Wheater <stuart.wheater@arjuna.com>
2020-08-21 15:46:45 +0100

    Refinement of tests

--- tests/testthat/test-smk-asFactorDS2.R
+++ tests/testthat/test-smk-asFactorDS2.R
@@ -22,8 +22,8 @@ set.standard.disclosure.settings()
 
 context("asFactorDS2::smk::simple")
 test_that("simple asFactorDS2, fixed.dummy.vars is FALSE", {
-    input                      <- c(2.0, 1.0, 3.0, 3.0, 3.0, 1.0, 2.0, 2.0, 1.0, 2.0)
-    all.unique.levels.transmit <- "1.0,2.0,3.0,4.0"
+    input                      <- c(2, 1, 3, 3, 3, 1, 2, 2, 1, 2)
+    all.unique.levels.transmit <- "1,2,3,4"
     fixed.dummy.vars           <- FALSE
     baseline.level             <- NULL
 
@@ -37,15 +37,15 @@ test_that("simple asFactorDS2, fixed.dummy.vars is FALSE", {
     expect_equal(class(res.levels), "character")
     expect_length(res.levels, 4)
 
-    expect_equal(res.levels[1], "1.0")
-    expect_equal(res.levels[2], "2.0")
-    expect_equal(res.levels[3], "3.0")
-    expect_equal(res.levels[4], "4.0")
+    expect_equal(res.levels[1], "1")
+    expect_equal(res.levels[2], "2")
+    expect_equal(res.levels[3], "3")
+    expect_equal(res.levels[4], "4")
 })
 
 test_that("simple asFactorDS2, fixed.dummy.vars is TRUE", {
-    input                      <- c(2.0, 1.0, 3.0, 3.0, 3.0, 1.0, 2.0, 2.0, 1.0, 2.0)
-    all.unique.levels.transmit <- "1.0,2.0,3.0,4.0"
+    input                      <- c(2, 1, 3, 3, 3, 1, 2, 2, 1, 2)
+    all.unique.levels.transmit <- "1,2,3,4"
     fixed.dummy.vars           <- TRUE
     baseline.level             <- 1.0
 
@@ -58,36 +58,36 @@ test_that("simple asFactorDS2, fixed.dummy.vars is TRUE", {
     expect_true("matrix" %in% res.class)
     expect_true("array" %in% res.class)
 
-    expect_equal(res[1], 0.0)
-    expect_equal(res[2], 0.0)
-    expect_equal(res[3], 0.0)
-    expect_equal(res[4], 0.0)
-    expect_equal(res[5], 0.0)
-    expect_equal(res[6], 0.0)
-    expect_equal(res[7], 0.0)
-    expect_equal(res[8], 0.0)
-    expect_equal(res[9], 0.0)
-    expect_equal(res[10], 0.0)
-    expect_equal(res[11], 0.0)
-    expect_equal(res[12], 0.0)
-    expect_equal(res[13], 0.0)
-    expect_equal(res[14], 0.0)
-    expect_equal(res[15], 0.0)
-    expect_equal(res[16], 0.0)
-    expect_equal(res[17], 0.0)
-    expect_equal(res[18], 0.0)
-    expect_equal(res[19], 0.0)
-    expect_equal(res[20], 0.0)
-    expect_equal(res[21], 0.0)
-    expect_equal(res[22], 0.0)
-    expect_equal(res[23], 0.0)
-    expect_equal(res[24], 0.0)
-    expect_equal(res[25], 0.0)
-    expect_equal(res[26], 0.0)
-    expect_equal(res[27], 0.0)
-    expect_equal(res[28], 0.0)
-    expect_equal(res[29], 0.0)
-    expect_equal(res[30], 0.0)
+    expect_equal(res[1], 1)
+    expect_equal(res[2], 0)
+    expect_equal(res[3], 0)
+    expect_equal(res[4], 0)
+    expect_equal(res[5], 0)
+    expect_equal(res[6], 0)
+    expect_equal(res[7], 1)
+    expect_equal(res[8], 1)
+    expect_equal(res[9], 0)
+    expect_equal(res[10], 1)
+    expect_equal(res[11], 0)
+    expect_equal(res[12], 0)
+    expect_equal(res[13], 1)
+    expect_equal(res[14], 1)
+    expect_equal(res[15], 1)
+    expect_equal(res[16], 0)
+    expect_equal(res[17], 0)
+    expect_equal(res[18], 0)
+    expect_equal(res[19], 0)
+    expect_equal(res[20], 0)
+    expect_equal(res[21], 0)
+    expect_equal(res[22], 0)
+    expect_equal(res[23], 0)
+    expect_equal(res[24], 0)
+    expect_equal(res[25], 0)
+    expect_equal(res[26], 0)
+    expect_equal(res[27], 0)
+    expect_equal(res[28], 0)
+    expect_equal(res[29], 0)
+    expect_equal(res[30], 0)
 
 
     res.colnames <- colnames(res)
@@ -95,9 +95,9 @@ test_that("simple asFactorDS2, fixed.dummy.vars is TRUE", {
     expect_equal(class(res.colnames), "character")
     expect_length(res.colnames, 3)
 
-    expect_equal(res.colnames[1], "DV2.0")
-    expect_equal(res.colnames[2], "DV3.0")
-    expect_equal(res.colnames[3], "DV4.0")
+    expect_equal(res.colnames[1], "DV2")
+    expect_equal(res.colnames[2], "DV3")
+    expect_equal(res.colnames[3], "DV4")
 })
 
 #
