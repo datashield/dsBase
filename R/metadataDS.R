6d275b18d5cc64b1d1a7342becf43329e0375307
Stuart Wheater <stuart.wheater@arjuna.com>
2020-11-03 12:50:03 +0000

    Added metadataDS function

---
+++ R/metadataDS.R
@@ -0,0 +1,33 @@
+#'
+#' @title Returns the metadata, if any, about the specified variable
+#' @description This function returns metadata, if any, about specified variable.
+#' @details The function returns the metadata, obtained from attributes function.
+#' @param x a string character, containing the name  of the specified variable
+#' @return a list containing the metadata. The elements of the list will depend
+#' on the meatadata available.
+#' @author Stuart Wheater, for DataSHIELD Development Team
+#' @export
+#'
+metadataDS <- function(x)
+{
+
+    if (is.null(x))
+        stop("Variable's name can't be NULL", call. = FALSE)
+
+    if ((! is.character(x)) || (length(x) != 1))
+        stop("Variable's name isn't be single character vector", call. = FALSE)
+
+    x.var <- eval(parse(text=x), envir = parent.frame())
+
+    if (! exists(x.var))
+        stop(paste0("The variable '", x, "' does not exist"), call. = FALSE)
+
+    # find the metadata specified variable
+    metadata <- attributes(x.var)
+
+    # return metadata
+    return(metadata)
+
+}
+#AGGREGATE FUNCTION
+# metadataDS
