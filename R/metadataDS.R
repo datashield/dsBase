697bee123c4685b33cd47a603dd32eb7761e5c8d
Stuart Wheater <stuart.wheater@arjuna.com>
2021-08-26 12:46:18 +0100

    Updated metadata

--- R/metadataDS.R
+++ R/metadataDS.R
@@ -10,24 +10,29 @@
 #'
 metadataDS <- function(x)
 {
-
     if (is.null(x))
         stop("Variable's name can't be NULL", call. = FALSE)
 
     if ((! is.character(x)) || (length(x) != 1))
         stop("Variable's name isn't be single character vector", call. = FALSE)
 
-    # if (! exists(x))
-    #    stop(paste0("The variable '", x, "' does not exist"), call. = FALSE)
+#    if (! exists(x, envir = parent.frame()))
+#        stop(paste0("The variable '", x, "' does not exist"), call. = FALSE)
 
     x.var <- eval(parse(text=x), envir = parent.frame())
 
     # find the metadata specified variable
-    metadata <- attributes(x.var)
+    metadata_attributes       <- attributes(x.var)
+    metadata_attributes_names <- names(metadata_attributes)
+    permitted_attribute_names <- c('names', 'spec', 'class', 'label', 'opal.value_type', 'opal.entity_type', 'opal.repeatable', 'opal.index', 'opal.nature')
+
+    metadata <- list()
+    for (permitted_attribute_name in permitted_attribute_names)
+        if (permitted_attribute_name %in% metadata_attributes_names)
+            metadata[[permitted_attribute_name]] <- metadata_attributes[[permitted_attribute_name]]
 
     # return metadata
     return(metadata)
-
 }
 #AGGREGATE FUNCTION
 # metadataDS
