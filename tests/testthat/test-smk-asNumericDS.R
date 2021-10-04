906f721ca5036d4b9f3770e82f663a32ee467739
Stuart Wheater <stuart.wheater@arjuna.com>
2021-10-04 14:18:15 +0100

    Extender tests

--- tests/testthat/test-smk-asNumericDS.R
+++ tests/testthat/test-smk-asNumericDS.R
@@ -44,6 +44,53 @@ test_that("character vector asNumericDS", {
     expect_equal(res[5], 505)
 })
 
+context("asNumericDS::smk::character 'non numeric' vector")
+test_that("character 'non numeric' vector asNumericDS", {
+    input <- c("aa", "bb", "cc", "dd", "ee")
+
+    res <- expect_warning(asNumericDS("input"), "NAs introduced by coercion", fixed = TRUE)
+
+    expect_length(res, 5)
+    expect_equal(class(res), "numeric")
+    expect_true(is.na(res[1]))
+    expect_true(is.na(res[2]))
+    expect_true(is.na(res[3]))
+    expect_true(is.na(res[4]))
+    expect_true(is.na(res[5]))
+})
+
+context("asNumericDS::smk::factor vector")
+test_that("factor vector asNumericDS", {
+    vec   <- c("101", "202", "303", "404", "505")
+    input <- as.factor(vec)
+
+    res <- asNumericDS("input")
+
+    expect_length(res, 5)
+    expect_equal(class(res), "numeric")
+    expect_equal(res[1], 101)
+    expect_equal(res[2], 202)
+    expect_equal(res[3], 303)
+    expect_equal(res[4], 404)
+    expect_equal(res[5], 505)
+})
+
+context("asNumericDS::smk::factor rev vector")
+test_that("factor vector asNumericDS", {
+    vec   <- c("505", "404", "303", "202", "101")
+    input <- as.factor(vec)
+
+    res <- asNumericDS("input")
+
+    expect_length(res, 5)
+    expect_equal(class(res), "numeric")
+    expect_equal(res[1], 505)
+    expect_equal(res[2], 404)
+    expect_equal(res[3], 303)
+    expect_equal(res[4], 202)
+    expect_equal(res[5], 101)
+})
+
 #
 # Done
 #
