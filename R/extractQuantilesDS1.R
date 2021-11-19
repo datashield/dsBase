bb8853551c0d7bd3939920c2ea034fa9b0d22228
Paul Burton <npb102@newcastle.ac.uk>
2021-11-19 16:53:35 +0000

    Commit all code and related files for the novel serverside functions called by ds.ranksSecure and ds.extractQuantiles

---
+++ R/extractQuantilesDS1.R
@@ -0,0 +1,134 @@
+#' @title Secure ranking of "V2BR" (vector to be ranked) across all sources
+#' and use of these ranks to estimate global quantiles across all studies
+#' @description identify the global values of V2BR (i.e. the values across all
+#' studies) that relate to a set of quantiles to be evaluated.
+#' @details Severside aggregate function called by ds.extractQuantiles via
+#' ds.ranksSecure. As well as estimating the key values of V2BR that correspond
+#' to the selected quantiles, this function also implements a disclosure control
+#' trap. If the ratio of the total number of all observations across all studies
+#' divided by the number of quantile values to be estimated is less than or
+#' equal to nfilter.subset (which specifies the minimum size of a subset) the
+#' process stops and an error message is returned suggesting that you might
+#' try selecting a narrower range of quantiles with less quantile values to be
+#' estimated as specified by the argument <quantiles.for.estimation> of the
+#' function ds.ranksSecure. For more details about the cluster of functions that
+#' collectively enable secure global ranking and estimation of global quantiles
+#' see the associated document entitled "secure.global.ranking.docx"
+#' @param extract.quantiles one of a restricted set of character strings that
+#' fix the set of quantile values for which the corresponding values across
+#' all studies are to be estimated. For more details
+#' see the associated document entitled "secure.global.ranking.docx", the
+#' header for ds.ranksSecure and ds.extractQuantiles functions.
+#' The value of this argument is set in choosing the value of the argument
+#' <quantiles.for.estimation> in ds.ranksSecure.
+#' @param extract.output.ranks.df a character string which specifies an optional
+#' name for the data.frame written to the serverside on each data source that
+#' contains 11 of the key output variables from the ranking procedure pertaining
+#' to that particular data source. This data frame represents the key source of
+#' information - including global ranks - that determines the values of V2BR
+#' that are identified as corresponding to the particular set of quantiles to
+#' be estimated as specified by the <quantiles.for.estimation> argument of
+#' function ds.ranksSecure (and the <extract.quantiles> argument of
+#' ds.extractQuantiles).
+#' @return as a first step in creating the vector of values of values of V2BR
+#' that correspond to each quantile value, extractQuantilesDS1 identifies the
+#' two closest quantile values across all studies that span each key quantile
+#' value. These are saved as the data frame "closest.bounds.df" on the
+#' clientside and then saved on the serverside by ds.dmtC2S into the data frame
+#' "global.bounds.df". Also if the number of observations across all studies is
+#' too small, and a  disclosure risk exists if the final.quantile.vector is made
+#' available via the client, this function stops the processing and returns a
+#' warning/error message.  
+#' @author Paul Burton 11th November, 2021
+#' @export
+extractQuantilesDS1 <- function(extract.quantiles,extract.output.ranks.df){ #START FUNC
+
+  
+  #############################################################
+  #MODULE 1: CAPTURE THE nfilter SETTINGS                     #
+  thr<-dsBase::listDisclosureSettingsDS()                     #
+  nfilter.tab <- as.numeric(thr$nfilter.tab)                 #
+  #nfilter.glm <- as.numeric(thr$nfilter.glm)                 #
+  nfilter.subset <- as.numeric(thr$nfilter.subset)           #
+  #nfilter.string <- as.numeric(thr$nfilter.string)           #
+  #nfilter.stringShort <- as.numeric(thr$nfilter.stringShort) #
+  #nfilter.kNN <- as.numeric(thr$nfilter.kNN)                 #
+  #nfilter.noise <- as.numeric(thr$nfilter.noise)             #
+  #nfilter.levels <- as.numeric(thr$nfilter.levels)            #
+  #############################################################
+ 
+  #############################################   
+ 
+  extract.ranks.df <- eval(parse(text=extract.output.ranks.df), envir = parent.frame())
+  
+ #Specify quantiles to evaluate
+  if(extract.quantiles=="0.025-0.975"){
+  evaluation.quantiles<-c(0.025,0.05,0.10,0.20,0.25,0.30,0.3333,0.40,0.50,0.60,0.6667,0.70,0.75,0.80,0.90,0.95,0.975)
+  }
+  if(extract.quantiles=="0.05-0.95"){
+    evaluation.quantiles<-c(0.05,0.10,0.20,0.25,0.30,0.3333,0.40,0.50,0.60,0.6667,0.70,0.75,0.80,0.90,0.95)
+  }
+  if(extract.quantiles=="0.10-0.90"){
+    evaluation.quantiles<-c(0.10,0.20,0.25,0.30,0.3333,0.40,0.50,0.60,0.6667,0.70,0.75,0.80,0.90)
+  }
+  if(extract.quantiles=="0.20-0.80"){
+    evaluation.quantiles<-c(0.20,0.25,0.30,0.3333,0.40,0.50,0.60,0.6667,0.70,0.75,0.80)
+  }
+  if(extract.quantiles=="quartiles"){
+    evaluation.quantiles<-c(0.25,0.50,0.75)
+  }
+  if(extract.quantiles=="median"){
+    evaluation.quantiles<-c(0.50)
+  }
+  
+  
+  numvals<-length(evaluation.quantiles)
+  numsubs.in.study<-nrow(extract.ranks.df)
+  
+  #calculate total number of real data observations in variable to be ranked
+  #across all studies to prime disclosure control
+
+  #Identify specific observations defining chosen evaluation quantiles
+  quants<-extract.ranks.df$final.quantiles.global
+   ranks<-extract.ranks.df$final.ranks.global
+   numsubs.real<-max(ranks/quants)
+   
+   if((numsubs.real/numvals)<=nfilter.tab){
+     error.message<-
+       paste0("FAILED: the total number of observations across all studies is so small that there is a disclosure risk in releasing the list of quantiles requested. You could change the quantiles.for.estimation argument to request a narrower range of quantiles to be be estimated.")
+     stop(error.message, call. = FALSE)
+   }
+   
+  
+  lower.bound<-rep(NA,numvals)
+  upper.bound<-rep(NA,numvals)
+  
+  num.real.all.studies<-max()
+  for(qq in 1:numvals){
+    disclosure.risk<-FALSE
+        if(length(quants<=evaluation.quantiles[qq])<nfilter.subset){
+          disclosure.risk<-TRUE
+        }  
+ 
+        if(length(quants>=evaluation.quantiles[qq])<nfilter.subset){
+        disclosure.risk<-TRUE
+        }  
+    
+    lower.bound[qq]<-max(quants[quants<=evaluation.quantiles[qq]])
+    upper.bound[qq]<-min(quants[quants>=evaluation.quantiles[qq]])
+
+  }
+  
+bounds.df<-data.frame(evaluation.quantiles,lower.bound,upper.bound)
+
+colnames(bounds.df)<-c("evaluation.quantiles","lower.bound","upper.bound")
+
+return(bounds.df)
+}
+
+#AGGREGATE
+# extractQuantilesDS1
+
+  
+
+
---
+++ R/extractQuantilesDS2.R
@@ -0,0 +1,95 @@
+#' @title Secure ranking of "V2BR" (vector to be ranked) across all sources
+#' and use of these ranks to estimate global quantiles across all studies
+#' @description identify the global values of V2BR (i.e. the values across all
+#' studies) that relate to a set of quantiles to be evaluated.
+#' @details Severside aggregate function called by ds.extractQuantiles via
+#' ds.ranksSecure. This takes the "global.bounds.df" data frame saved on the
+#' serverside following construction by extractQuantilesDS1. This
+#' data frame includes the two quantile values that most closely span each
+#' quartile value to be estimated. If either of the values had been the correct 
+#' value for a given quantile, both the bounding values would have taken that
+#' value in global.bounds.df. This is because the upper bound was defined as
+#' the lowest value that was equal to or greater than the true value for that
+#' quantile while the lower bound was defined as the highest value that was
+#' equal to or lower than the true value. Next, the function extractQuantileDS2
+#' goes round study by study to identify the values of V2BR that actually
+#' correspond to each of the spanning values around each quantile. Then the
+#' function goes quantile by quantile and estimates the mean of the
+#' two values of V2BR that correspond to the the spanning quantiles. If these
+#' two values are the same it means that that value of V2BR is the
+#' "true" value and the mean of two (or potentially several) instances of that
+#' value is inevitably also equal to that true value. If the upper and lower
+#' bounding values of V2BR differ, neither can  be the precisely correct single
+#' value of V2BR for that quantile (see above for explanation) and so the mean
+#' of the two is a reasonable interpolated summary.
+#' @param extract.output.ranks.df a character string which specifies an optional
+#' name for the data.frame written to the serverside on each data source that
+#' contains 11 of the key output variables from the ranking procedure pertaining
+#' to that particular data source. This data frame represents the key source of
+#' information - including global ranks - that determines the values of V2BR
+#' that are identified as corresponding to the particular set of quantiles to
+#' be estimated as specified by the <quantiles.for.estimation> argument of
+#' function ds.ranksSecure (and the <extract.quantiles> argument of
+#' ds.extractQuantiles).
+#' @return the single value of V2BR which best corresponds to each key quantile
+#' value to be estimated as specified by the argument <quantiles.for.estimation>
+#' A data frame (final.quantile.df)summarising the results of this analysis
+#' is written to the clientside.  This data frame consists of two vectors. The
+#' first is named "evaluation.quantiles". It lists the full set of quantiles
+#' you have requested for evaluation as specified by the argument
+#' "quantiles.for.estimation" The second vector which is called
+#' "final.quantile.vector" details the values of V2BR that correspond to the
+#' the key quantiles listed in vector 1.
+#' @author Paul Burton 11th November, 2021
+#' @export
+extractQuantilesDS2 <- function(extract.output.ranks.df){ #START FUNC
+
+  
+  #############################################################
+  #MODULE 1: CAPTURE THE nfilter SETTINGS                     #
+  thr<-dsBase::listDisclosureSettingsDS()                     #
+  #nfilter.tab <- as.numeric(thr$nfilter.tab)                 #
+  #nfilter.glm <- as.numeric(thr$nfilter.glm)                 #
+  nfilter.subset <- as.numeric(thr$nfilter.subset)           #
+  #nfilter.string <- as.numeric(thr$nfilter.string)           #
+  #nfilter.stringShort <- as.numeric(thr$nfilter.stringShort) #
+  #nfilter.kNN <- as.numeric(thr$nfilter.kNN)                 #
+  #nfilter.noise <- as.numeric(thr$nfilter.noise)             #
+  #nfilter.levels <- as.numeric(thr$nfilter.levels)            #
+  #############################################################
+
+extract.ranks.df.2 <- eval(parse(text=extract.output.ranks.df), envir = parent.frame())
+numsubs<-nrow(extract.ranks.df.2)
+numvals<-length(global.bounds.df$evaluation.quantiles)
+
+
+relevant.study.specific.input.values.lower<-rep(NA,numvals)
+relevant.study.specific.input.values.upper<-rep(NA,numvals)
+
+for(ww in 1:numvals){
+  if(sum(extract.ranks.df.2$final.quantiles.global==global.bounds.df$lower.bound.vector[ww])>0)
+  {
+    relevant.study.specific.input.values.lower[ww]<-
+    mean(extract.ranks.df.2$input.var.real.orig[extract.ranks.df.2$final.quantiles.global
+                                        ==global.bounds.df$lower.bound.vector[ww]])
+  }
+
+  if(sum(extract.ranks.df.2$final.quantiles.global==global.bounds.df$upper.bound.vector[ww])>0)
+  {
+  relevant.study.specific.input.values.upper[ww]<-
+    mean(extract.ranks.df.2$input.var.real.orig[extract.ranks.df.2$final.quantiles.global
+                                        ==global.bounds.df$upper.bound.vector[ww]])
+  }
+}
+
+
+    return(list(relevant.study.specific.input.values.lower=relevant.study.specific.input.values.lower,
+                relevant.study.specific.input.values.upper=relevant.study.specific.input.values.upper))
+}  
+
+#AGGREGATE
+# extractQuantilesDS2
+
+  
+
+
