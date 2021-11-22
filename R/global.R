c9c77d66c9209b4292e45dbf71f1406a9782fbb3
Paul Burton <npb102@newcastle.ac.uk>
2021-11-22 09:47:01 +0000

    Update blackBoxDS and blackBoxRanksDS to address potential disclosure risk relating to risk of clientside deliberately understating the overal standard deviation of V2BR to make distribution of pseudodata too narrow. Also to clarify packages from which unspecified R functions are derived.

--- R/global.R
+++ R/global.R
@@ -1,4 +1,6 @@
 # Define the global variables 'offset.to.use', 'weights.to.use', 'out.table.real', 'out.table.dim', 'out.table.dimnames', 'list.obj' and 'mg'
 
 utils::globalVariables(c('offset.to.use', 'weights.to.use', 'out.table.real', 'out.table.dim', 'out.table.dimnames', 'list.obj', 'mg',
-                         'blackbox.output.df', 'blackbox.ranks.df', 'global.bounds.df', 'global.ranks.quantiles.df', 'sR4.df'))
+                         'blackbox.output.df', 'blackbox.ranks.df', 'global.bounds.df', 'global.ranks.quantiles.df', 'sR4.df',
+                         'min.max.df','sR5.df'))
+
