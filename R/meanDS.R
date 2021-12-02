fcb1737c8e07b15507d50fc7ca71d1b96083453f
tombisho <trpb2@cam.ac.uk>
2021-12-02 08:36:09 -0800

    first attempt at concentration disclosure control

--- R/meanDS.R
+++ R/meanDS.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 meanDS <- function(xvect){
-
+  
   #############################################################
   # MODULE 1: CAPTURE THE nfilter SETTINGS
   thr <- listDisclosureSettingsDS()
@@ -18,21 +18,33 @@ meanDS <- function(xvect){
   #nfilter.subset <- as.numeric(thr$nfilter.subset)
   #nfilter.string <- as.numeric(thr$nfilter.string)
   #############################################################
-
+  
   out.mean <- mean(xvect, na.rm=TRUE)
   out.numNa <- length(which(is.na(xvect)))
   out.totN <- length(xvect)
   out.validN <- out.totN-out.numNa
   studysideMessage <- "VALID ANALYSIS"
-
+  
   if(out.validN < nfilter.tab){
     out.mean <- NA
     stop("FAILED: Nvalid less than nfilter.tab", call. = FALSE)
   }
-
+  
+  
+  #############################################################
+  # New proposal for concentration                            #
+  #############################################################
+  concentration = 0.9
+  xtest = xvect/sum(xvect, na.rm = TRUE)
+  
+  if(any(xtest>concentration, na.rm=TRUE)){
+    out.mean <- NA
+    stop("FAILED: One value accounts for more than nfilter.conc of the mean", call. = FALSE)
+  }
+  
   out.obj <- list(EstimatedMean=out.mean,Nmissing=out.numNa,Nvalid=out.validN,Ntotal=out.totN,ValidityMessage=studysideMessage)
   return(out.obj)
-
+  
 }
 #AGGREGATE FUNCTION
 # meanDS
