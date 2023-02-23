2bb1fde78173c96d8b7f8a6ae1e9a43fd00fa92e
davraam <avraam.demetris@gmail.com>
2023-02-23 12:54:15 +0200

    additional tests for recodeValuesDS

--- tests/testthat/test-smk-recodeValuesDS.R
+++ tests/testthat/test-smk-recodeValuesDS.R
@@ -20,7 +20,7 @@ set.standard.disclosure.settings()
 # Tests
 #
 
-test_that("simple recodeValuesDS", {
+test_that("simple recodeValuesDS, numeric input, with no missings", {
     input          <- c(1, 2, 3, 4, 1, 3)
     values2replace <- "1,3"
     new.values     <- "10,30"
@@ -35,6 +35,91 @@ test_that("simple recodeValuesDS", {
     expect_equal(res[4], 4)
     expect_equal(res[5], 10)
     expect_equal(res[6], 30)
+})
+
+test_that("simple recodeValuesDS, numeric input, with missings", {
+  input          <- c(1, 2, 3, 4, 1, 3, NA)
+  values2replace <- "1,3"
+  new.values     <- "10,30"
+  missing        <- NULL
+  
+  res <- recodeValuesDS("input", values2replace, new.values, missing)
+  
+  expect_equal(class(res), "numeric")
+  expect_length(res, 7)
+  expect_equal(res[1], 10)
+  expect_equal(res[2], 2)
+  expect_equal(res[3], 30)
+  expect_equal(res[4], 4)
+  expect_equal(res[5], 10)
+  expect_equal(res[6], 30)
+  expect_true(is.na(res[7]))
+})
+
+test_that("simple recodeValuesDS, numeric input with missings, replace missings", {
+  input          <- c(1, 2, 3, 4, 1, 3, NA)
+  values2replace <- "1,3"
+  new.values     <- "10,30"
+  missing        <- "999"
+  
+  res <- recodeValuesDS("input", values2replace, new.values, missing)
+  
+  expect_equal(class(res), "numeric")
+  expect_length(res, 7)
+  expect_equal(res[1], 10)
+  expect_equal(res[2], 2)
+  expect_equal(res[3], 30)
+  expect_equal(res[4], 4)
+  expect_equal(res[5], 10)
+  expect_equal(res[6], 30)
+  expect_equal(res[7], 999)
+  expect_true(length(which(is.na(res)))==0)
+})
+
+test_that("simple recodeValuesDS, factor input, with no missings", {
+  input          <- as.factor(c(1, 2, 3, 4, 1, 3))
+  values2replace <- "1,3"
+  new.values     <- "10,30"
+  expected_output <- as.factor(c(10, 2, 30, 4, 10, 30))
+
+  res <- recodeValuesDS("input", values2replace, new.values)
+  
+  expect_equal(class(res), "factor")
+  expect_length(res, 6)
+  expect_true(setequal(levels(res), levels(expected_output)))
+})
+
+test_that("simple recodeValuesDS, factor input with missings, replace missings", {
+  input          <- as.factor(c(1, 2, 3, 4, 1, 3, NA))
+  values2replace <- "1,3"
+  new.values     <- "10,30"
+  missing        <- "999"
+  expected_output <- as.factor(c(10, 2, 30, 4, 10, 30, 999))
+  
+  res <- recodeValuesDS("input", values2replace, new.values, missing)
+  
+  expect_equal(class(res), "factor")
+  expect_length(res, 7)
+  expect_true(setequal(levels(res), levels(expected_output)))
+  expect_true(length(which(is.na(res)))==0)
+})
+
+test_that("simple recodeValuesDS, character input with missings", {
+  input          <- c('ab', 'ab', 'ba', 'ba', 'bb', 'aa', NA)
+  values2replace <- "ab,ba"
+  new.values     <- "cc,dd"
+
+  res <- recodeValuesDS("input", values2replace, new.values)
+  
+  expect_equal(class(res), "character")
+  expect_length(res, 7)
+  expect_equal(res[1], 'cc')
+  expect_equal(res[2], 'cc')
+  expect_equal(res[3], 'dd')
+  expect_equal(res[4], 'dd')
+  expect_equal(res[5], 'bb')
+  expect_equal(res[6], 'aa')
+  expect_true(is.na(res[7]))
 })
 
 #
