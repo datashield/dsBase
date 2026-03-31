df77a45082ed24e620fab0869a2429564dbb574c
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-03-31 13:40:39 +0200

    refactor: replace eval(parse()) with .loadServersideObject()

--- R/namesDS.R
+++ R/namesDS.R
@@ -50,14 +50,14 @@ nfilter.stringShort<-as.numeric(thr$nfilter.stringShort)                #
     stop(studysideMessage, call. = FALSE)
   }
 
-  list.obj<-eval(parse(text=xname.transmit), envir = parent.frame())
-
-  trace.message<-class(list.obj)
-
+  list.obj <- .loadServersideObject(xname.transmit)
 
   if(!is.list(list.obj)){
-    error.message <- "The input object is not of class <list>"
-    stop(paste0(error.message,trace.message), call. = FALSE)
+    stop(
+      "The input object is not of class <list>. '", xname.transmit, "' is type ",
+      paste(class(list.obj), collapse = ", "),
+      call. = FALSE
+    )
   }
 
 
